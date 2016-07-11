64bce9d57a9e1d6cb18abc62b02ccfa66cb8e2b8
Luke Cartledge <luke@lukecartledge.com>
2016-07-11 18:14:01 +0100

    passes 05 - adds top_up allowing balance to be topped up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,9 @@ describe Oystercard do
     it 'should initialize with 0' do
       expect(subject.balance).to eq 0
     end
+    it 'should update the balance with top_up' do
+      expect{ subject.top_up(10) }.to change{ subject.balance }.by 10
+    end
   end
 
 end
