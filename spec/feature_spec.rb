2958a6725561aee55c9467ce3b0dcee92d30848e
samjbro <sjbroughton1@gmail.com>
2016-07-11 21:39:35 +0100

    Challenge #5: 'Enable top up functionality' complete

---
+++ spec/feature_spec.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/oystercard.rb'
+
+card = Oystercard.new
+card.top_up 10
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard  do
 	it 'has a default initial balance of 0' do
 		expect(subject.balance).to eq 0
 	end
+	describe '#top_up' do
+		it { is_expected.to respond_to(:top_up).with(1).argument }
+		it 'can top up the balance' do
+			subject.top_up 10
+			expect(subject.balance).to eq 10
+		end
+	end
 end
