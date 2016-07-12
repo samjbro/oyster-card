2f40b9d2967e6b27a30e24466e96a8b503fb754d
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-12 11:22:08 +0100

    add top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,10 @@
 class Oystercard
-  attr_reader :balance
+  attr_accessor :balance
 
   def initialize
     @balance = 0
   end
-  
+  def top_up(amount)
+    self.balance += amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,13 @@ describe Oystercard do
     expect(subject).to receive(:balance)
     subject.balance
   end
+
+  context '#top_up' do
+
+    it {is_expected.to respond_to(:top_up).with(1).arguments}
+
+    it 'should be able to top up balance' do
+      expect{ subject.top_up(10) }.to change{ subject.balance }.by(10)
+    end
+  end
 end
